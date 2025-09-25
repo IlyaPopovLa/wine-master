3a9f2233500beea71f3b91b02425c60c3406a7bb
IlyaPopovLa <il.popov@lamoda.ru>
2025-09-25 21:45:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+from dotenv import load_dotenv
 import datetime
 import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -5,16 +8,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+START_DAY = datetime.datetime(year=1904, month=1, day=1, hour=0)
 
-template = env.get_template('template.html')
-
-start_day = datetime.datetime(year=1904, month=1, day=1, hour=0)
-today = datetime.datetime.today()
-delta_year = today.year - start_day.year
 
 def get_year_form(delta_year):
     if delta_year % 100 in [11, 12, 13, 14]:
@@ -27,44 +22,78 @@ def get_year_form(delta_year):
     else:
         return 'лет'
 
-year_form = get_year_form(delta_year)
 
-wine_table = pandas.read_excel('wine3.xlsx', sheet_name='Лист1')
+def load_wine_data(excel_file_path):
+    wine_table = pandas.read_excel(excel_file_path, sheet_name='Лист1')
 
-wine_table = wine_table.rename(columns={
-    'Категория': 'category',
-    'Название': 'title',
-    'Сорт': 'variety',
-    'Цена': 'price',
-    'Картинка': 'image',
-    'Акция': 'promo'
-})
+    wine_table = wine_table.rename(columns={
+        'Категория': 'category',
+        'Название': 'title',
+        'Сорт': 'variety',
+        'Цена': 'price',
+        'Картинка': 'image',
+        'Акция': 'promo'
+    })
 
-wine_table['image'] = 'images/' + wine_table['image'].astype(str)
-wine_table['variety'] = wine_table['variety'].where(wine_table['variety'].notna(), None)
-wine_table['promo'] = wine_table['promo'].fillna('').str.strip()
+    wine_table['image'] = 'images/' + wine_table['image'].astype(str)
+    wine_table['variety'] = wine_table['variety'].where(wine_table['variety'].notna(), None)
+    wine_table['promo'] = wine_table['promo'].fillna('').str.strip()
 
-wines_category = defaultdict(list)
+    wines_category = defaultdict(list)
+    for row in wine_table.itertuples(index=False):
+        category = row.category.strip().lower()
+        wine_info = {
+            'title': row.title,
+            'variety': row.variety,
+            'price': row.price,
+            'image': row.image,
+            'promo': row.promo == 'Выгодное предложение'
+        }
+        wines_category[category].append(wine_info)
 
-for row in wine_table.itertuples(index=False):
-    category = row.category.strip().lower()
-    wine_info = {
-        'title': row.title,
-        'variety': row.variety,
-        'price': row.price,
-        'image': row.image,
-        'promo': row.promo == 'Выгодное предложение'
-    }
-    wines_category[category].append(wine_info)
+    return wines_category
 
-rendered_page = template.render(
-    wines_category=wines_category,
-    delta_year=delta_year,
-    year_form=year_form
-)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def render_site(wines_category, delta_year, year_form):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        wines_category=wines_category,
+        delta_year=delta_year,
+        year_form=year_form
+    )
+    with open('index.html', 'w', encoding='utf-8') as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+def run_server():
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Wine shop site generator')
+    parser.add_argument('--data-path', default=os.getenv('WINE_DATA_PATH', 'data_wine.xlsx'),
+    help='Path to Excel data file (default: data_wine.xlsx or .env WINE_DATA_PATH)')
+    return parser.parse_args()
+
+
+def main():
+    load_dotenv()
+    args = parse_args()
+    data_path = args.data_path
+
+    today = datetime.datetime.today()
+    delta_year = today.year - START_DAY.year
+    year_form = get_year_form(delta_year)
+
+    wines_category = load_wine_data(data_path)
+    render_site(wines_category, delta_year, year_form)
+    run_server()
+
+
+if __name__ == '__main__':
+    main()
